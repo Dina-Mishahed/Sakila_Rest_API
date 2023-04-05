59e9465f4f1759813012bbfa4976bbfbed4f50a6
Dina Mishahed <53543119+Dina-Mishahed@users.noreply.github.com>
2023-04-05 21:33:57 +0200

    generate classes

---
+++ src/main/java/gov/iti/jets/SOAP_vs_REST_APIs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gov.iti.jets;
+
+/**
+ *
+ * @author hp
+ */
+public class SOAP_vs_REST_APIs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/gov/iti/jets/entity/SalesByFilmCategory.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gov.iti.jets.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author hp
+ */
+@Entity
+@Table(name = "sales_by_film_category")
+@NamedQueries({
+    @NamedQuery(name = "SalesByFilmCategory.findAll", query = "SELECT s FROM SalesByFilmCategory s"),
+    @NamedQuery(name = "SalesByFilmCategory.findByCategory", query = "SELECT s FROM SalesByFilmCategory s WHERE s.category = :category"),
+    @NamedQuery(name = "SalesByFilmCategory.findByTotalSales", query = "SELECT s FROM SalesByFilmCategory s WHERE s.totalSales = :totalSales")})
+public class SalesByFilmCategory implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Basic(optional = false)
+    @Column(name = "category")
+    private String category;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "total_sales")
+    private BigDecimal totalSales;
+
+    public SalesByFilmCategory() {
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(BigDecimal totalSales) {
+        this.totalSales = totalSales;
+    }
+    
+}
